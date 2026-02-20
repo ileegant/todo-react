bb06dbbe7cea74607500064c7149c165c5b9686b
ileegant <osukhoruchko@gmail.com>
2026-02-20 16:09:45 +0200

    Add initial todos constants to constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const initial_todos = [
+  { id: 1, content: "FIRST TODOS", isComplited: false },
+  { id: 2, content: "SECOND TODOS", isComplited: false },
+  { id: 3, content: "THIRD TODOS", isComplited: false },
+];

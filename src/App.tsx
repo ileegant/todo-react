c1dc0067105e79962888b6cddc47483a65ba3010
ileegant <osukhoruchko@gmail.com>
2026-02-20 16:26:04 +0200

    Add functionality to create and add new todos in TodoForm component

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,23 @@ import { initial_todos } from "./lib/constants";
 function App() {
   const [todos, setTodos] = useState<Todo[]>(initial_todos);
 
+  function handleAddTodo(value: string) {
+    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+
+    const newTodo = {
+      id: newId,
+      content: value,
+      isComplited: false,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+  }
+
   return (
     <div className="relative min-h-screen w-full flex justify-center items-center bg-white overflow-hidden">
       <div className="flex justify-around bg-gray-100 border-2 border-gray-200 rounded-sm w-[940px] h-[620px]">
         <TodoList todos={todos} />
-        <TodoForm />
+        <TodoForm onAdd={handleAddTodo} />
       </div>
     </div>
   );
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,12 +1,28 @@
-export default function TodoForm() {
+import { useState } from "react";
+
+interface TodoFormProps {
+  onAdd: (value: string) => void;
+}
+
+export default function TodoForm({ onAdd }: TodoFormProps) {
+  const [value, setValue] = useState("");
+
   return (
     <div className="flex flex-1 flex-col space-y-4 items-center text-gray-600 p-6 bg-gray-200">
       <h2 className="text-2xl font-black">CREATE TODO</h2>
       <textarea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         className="w-full max-w-lg min-h-[120px] p-2 border border-gray-400 rounded-sm text-slate-800 resize-none"
         placeholder="Write here.."
       />
-      <button className="w-full border-2 py-1 font-bold border-gray-600 cursor-pointer hover:bg-gray-600 hover:text-gray-200">
+      <button
+        className="w-full border-2 py-1 font-bold border-gray-600 cursor-pointer hover:bg-gray-600 hover:text-gray-200"
+        onClick={() => {
+          onAdd(value);
+          setValue("");
+        }}
+      >
         ADD TODO
       </button>
     </div>
